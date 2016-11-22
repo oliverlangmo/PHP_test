b3847db9a5a488f478074522067f3a5f3e2c6987
oliverlangmo <oliver.langmo@gmail.com>
2016-11-22 12:46:28 -0600

    learning basic PHP

---
+++ variable.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+ $name = 'Oliver';
+ $number = 3;
+ echo $name . " " . $number;
+
+    ?>
+  </body>
+</html>
